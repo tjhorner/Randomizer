9f1db918da9c43a78b5c0042b24bf0d0d513400d
GeekyGamer14 <tj.201@me.com>
2013-11-10 13:26:15 -0800

    bootons

--- index.php
+++ index.php
@@ -20,6 +20,9 @@
 	<code><?php echo($randString); ?></code><br/>
 	Using <code>randomString(16, 'upper');</code>, a random string with 16 characters that are uppercase will be shown.<br/>
 	<code><?php echo($randStringU); ?></code><br/>
-	<a href="#" onclick="location.reload()"><button>Reload</button></a>
+	<a href="#" onclick="location.reload()"><button>Reload</button></a><br/>
+	<a href="https://github.com/GeekyGamer14/Randomizer/archive/master.zip">download randomizer</a><br/>
+	<a href="https://github.com/GeekyGamer14/Randomizer">view code on github</a><br/>
+	<iframe src="http://githubbadge.appspot.com/GeekyGamer14" style="border: 0;height: 142px;width: 200px;overflow: hidden;" frameBorder="0"></iframe>
 </body>
 </html>
