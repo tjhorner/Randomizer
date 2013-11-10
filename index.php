7bd20439940f5c77093529d7633a728e18b84de4
GeekyGamer14 <tj.201@me.com>
2013-11-10 13:38:15 -0800

    customize number of characters

--- index.php
+++ index.php
@@ -8,19 +8,27 @@
 		'fur',
 	);
 	$randArray = $r -> randomArray($a);
-	$randString = $r -> randomString(16, 'normal');
-	$randStringU = $r -> randomString(16, 'upper');
+	if(isset($_GET['chars'])){
+		$chars = $_GET['chars'];
+	}else{
+		$chars = 16;
+	}
+	$randString = $r -> randomString($chars, 'normal');
+	$randStringU = $r -> randomString($chars, 'upper');
 ?>
 <html>
 <title>Randomizer live demo</title>
 <body>
-	Using <code>randomArray($a);</code>, a random object from the array $a will be shown.<br/>
-	<code><?php echo($randArray); ?></code><br/>
-	Using <code>randomString(16);</code>, a random string with 16 characters will be shown.<br/>
-	<code><?php echo($randString); ?></code><br/>
-	Using <code>randomString(16, 'upper');</code>, a random string with 16 characters that are uppercase will be shown.<br/>
-	<code><?php echo($randStringU); ?></code><br/>
-	<a href="#" onclick="location.reload()"><button>Reload</button></a><br/>
+	<form>
+		Using <code>randomArray($a);</code>, a random object from the array $a will be shown.<br/>
+		<code><?php echo($randArray); ?></code><br/>
+		Using <code>randomString(16);</code>, a random string with 16 characters will be shown.<br/>
+		<code><?php echo($randString); ?></code><br/>
+		Using <code>randomString(16, 'upper');</code>, a random string with 16 characters that are uppercase will be shown.<br/>
+		<code><?php echo($randStringU); ?></code><br/>
+		# of characters: <input name="chars" value="<?php echo($chars) ?>"></input><br/>
+		<a href="#"><button>Reload</button></a><br/>
+	</form>
 	<a href="https://github.com/GeekyGamer14/Randomizer/archive/master.zip">download randomizer</a><br/>
 	<a href="https://github.com/GeekyGamer14/Randomizer">view code on github</a><br/>
 	<iframe src="http://githubbadge.appspot.com/GeekyGamer14" style="border: 0;height: 142px;width: 200px;overflow: hidden;" frameBorder="0"></iframe>
